7e14aaedc2909b62075bb16c35787df3c6311b1b
neenad <neenadkambli@gmail.com>
2021-02-14 15:37:36 +0530

    modified

--- routes/memeRoute.js
+++ routes/memeRoute.js
@@ -4,16 +4,18 @@ var mongoose = require('mongoose');
 const Memes = require('../models/mememodel');
 const app = express.Router();
 
-
-// function isURL(str) {
-//     var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-//     '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
-//     '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-//     '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-//     '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-//     '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-//     return pattern.test(str);
-//   }
+function is_url(str)
+{
+  regexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+        if (regexp.test(str))
+        {
+          return true;
+        }
+        else
+        {
+          return false;
+        }
+}
 
 app.route('/')
 .get((req,res)=>{
@@ -38,10 +40,10 @@ app.route('/')
             res.json({status:'fail',message:'duplicate post of meme not allowed'});
         }
         else{
-            // if(!isURL(req.body.url)){
-            //     res.statusCode=404;
-            //     res.json({status:'fail',message:'please input a valid url'});
-            // }
+            if(!is_url(req.body.url)){
+                res.statusCode=404;
+                res.json({status:'fail',message:'please input a valid url'});
+            }
                   Memes.create(req.body)
                   .then((Meme)=>{
                       console.log('successfully uploaded meme\n',{"id":Meme._id});
